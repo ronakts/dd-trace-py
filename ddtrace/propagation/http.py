1a30e3db31e4363d5bd85f1edede053b482c7b94
Tahir H. Butt <tahir.butt@datadoghq.com>
2022-04-18 12:58:02 -0400

    feat: re-add support for datadog tag propagation

--- ddtrace/propagation/http.py
+++ ddtrace/propagation/http.py
@@ -1,6 +1,8 @@
 from typing import Dict
 from typing import FrozenSet
 from typing import Optional
+from typing import Union
+from typing import cast
 
 from ddtrace import config
 
@@ -8,6 +10,12 @@ from ..constants import AUTO_KEEP
 from ..constants import AUTO_REJECT
 from ..constants import USER_KEEP
 from ..context import Context
+from ..internal._tagset import TagsetDecodeError
+from ..internal._tagset import TagsetEncodeError
+from ..internal._tagset import TagsetMaxSizeError
+from ..internal._tagset import decode_tagset_string
+from ..internal._tagset import encode_tagset_values
+from ..internal.compat import ensure_str
 from ..internal.compat import ensure_text
 from ..internal.constants import PROPAGATION_STYLE_B3
 from ..internal.constants import PROPAGATION_STYLE_B3_SINGLE_HEADER
@@ -36,12 +44,16 @@ def _possible_header(header):
     return frozenset([header, get_wsgi_header(header).lower()])
 
 
+HTTP_HEADER_TAGS = "x-datadog-tags"
+
+
 # Note that due to WSGI spec we have to also check for uppercased and prefixed
 # versions of these headers
 POSSIBLE_HTTP_HEADER_TRACE_IDS = _possible_header(HTTP_HEADER_TRACE_ID)
 POSSIBLE_HTTP_HEADER_PARENT_IDS = _possible_header(HTTP_HEADER_PARENT_ID)
 POSSIBLE_HTTP_HEADER_SAMPLING_PRIORITIES = _possible_header(HTTP_HEADER_SAMPLING_PRIORITY)
 POSSIBLE_HTTP_HEADER_ORIGIN = _possible_header(HTTP_HEADER_ORIGIN)
+POSSIBLE_HTTP_HEADER_TAGS = frozenset([HTTP_HEADER_TAGS, get_wsgi_header(HTTP_HEADER_TAGS).lower()])
 _POSSIBLE_HTTP_HEADER_B3_SINGLE_HEADER = _possible_header(_HTTP_HEADER_B3_SINGLE)
 _POSSIBLE_HTTP_HEADER_B3_TRACE_IDS = _possible_header(_HTTP_HEADER_B3_TRACE_ID)
 _POSSIBLE_HTTP_HEADER_B3_SPAN_IDS = _possible_header(_HTTP_HEADER_B3_SPAN_ID)
@@ -108,6 +120,35 @@ class _DatadogMultiHeader:
         if span_context.dd_origin is not None:
             headers[HTTP_HEADER_ORIGIN] = ensure_text(span_context.dd_origin)
 
+        # Do not try to encode tags if we have already tried and received an error
+        if "_dd.propagation_error" in span_context._meta:
+            return
+
+        # TODO: add whitelist
+        # Only propagate tags that start with `_dd.p.`
+        tags_to_encode = {}  # type: Dict[str, str]
+        for key, value in span_context._meta.items():
+            # DEV: encoding will fail if the key or value are not `str`
+            key = ensure_str(key)
+            if key.startswith("_dd.p."):
+                tags_to_encode[key] = ensure_str(value)
+
+        if tags_to_encode:
+            encoded_tags = None
+
+            try:
+                encoded_tags = encode_tagset_values(tags_to_encode)
+            except TagsetMaxSizeError:
+                # We hit the max size allowed, add a tag to the context to indicate this happened
+                span_context._meta["_dd.propagation_error"] = "max_size"
+                log.warning("failed to encode x-datadog-tags", exc_info=True)
+            except TagsetEncodeError:
+                # We hit an encoding error, add a tag to the context to indicate this happened
+                span_context._meta["_dd.propagation_error"] = "encoding_error"
+                log.warning("failed to encode x-datadog-tags", exc_info=True)
+            if encoded_tags:
+                headers[HTTP_HEADER_TAGS] = encoded_tags
+
     @staticmethod
     def _extract(headers):
         # type: (Dict[str, str]) -> Optional[Context]
@@ -131,6 +172,19 @@ class _DatadogMultiHeader:
             POSSIBLE_HTTP_HEADER_ORIGIN,
             headers,
         )
+        meta = None
+        tags_value = HTTPPropagator._extract_header_value(
+            POSSIBLE_HTTP_HEADER_TAGS,
+            headers,
+            default="",
+        )
+        if tags_value:
+            # Do not fail if the tags are malformed
+            try:
+                # We get a Dict[str, str], but need it to be Dict[Union[str, bytes], str] (e.g. _MetaDictType)
+                meta = cast(Dict[Union[str, bytes], str], decode_tagset_string(tags_value))
+            except TagsetDecodeError:
+                log.debug("failed to decode x-datadog-tags: %r", tags_value, exc_info=True)
 
         # Try to parse values into their expected types
         try:
@@ -145,14 +199,19 @@ class _DatadogMultiHeader:
                 span_id=int(parent_span_id) or None,  # type: ignore[arg-type]
                 sampling_priority=sampling_priority,  # type: ignore[arg-type]
                 dd_origin=origin,
+                meta=meta,
             )
         except (TypeError, ValueError):
             log.debug(
-                "received invalid x-datadog-* headers, " "trace-id: %r, parent-id: %r, priority: %r, origin: %r",
+                (
+                    "received invalid x-datadog-* headers, "
+                    "trace-id: %r, parent-id: %r, priority: %r, origin: %r, tags:%r"
+                ),
                 trace_id,
                 parent_span_id,
                 sampling_priority,
                 origin,
+                tags_value,
             )
         return None
 
@@ -422,6 +481,17 @@ class HTTPPropagator(object):
             _B3MultiHeader._inject(span_context, headers)
         if PROPAGATION_STYLE_B3_SINGLE_HEADER in config._propagation_style_inject:
             _B3SingleHeader._inject(span_context, headers)
+
+    @staticmethod
+    def _extract_header_value(possible_header_names, headers, default=None):
+        # type: (FrozenSet[str], Dict[str, str], Optional[str]) -> Optional[str]
+        for header in possible_header_names:
+            try:
+                return headers[header]
+            except KeyError:
+                pass
+
+        return default
 
     @staticmethod
     def extract(headers):
